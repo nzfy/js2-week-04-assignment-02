a3785ae40d52a5d820f7000828d9ae02b79fadaa
nzfy <nazifyalili34@gmail.com>
2022-12-06 02:00:39 +0100

    Homework has done

--- functions.js
+++ functions.js
@@ -1,5 +1,15 @@
 const isTheWordSplit = (wordSplit) => {
-
+  const newWord = wordSplit[0];
+  const item = wordSplit[1]
+          .split(",");
+            for (let i = 0; i < item.length; i++) {
+              for (let j = 0; j < item.length; j++) {
+                if (item[i] + item[j] == newWord) {
+                  return item[i] + "," + item[j];
+      }
+    }
+  }
+  return "not possible";
 };
 
 
